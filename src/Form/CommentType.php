b32675f53fe6defdd72c5c50035d1445d65bb05d
Clément Bartholomé <clementoss@gmail.com>
2023-10-18 19:30:51 +0200

    add form event to display username

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -5,10 +5,21 @@ namespace App\Form;
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bundle\SecurityBundle\Security ;
 
 class CommentType extends AbstractType
 {
+    private $user;
+
+    public function __construct(Security $security)
+    {
+        $this->user = $security->getUser();
+    }
+ 
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -24,6 +35,13 @@ class CommentType extends AbstractType
                     'placeholder' => 'Your name',
                 ],
             ]);
+
+            $builder->addEventListener(FormEvents::POST_SET_DATA, function (FormEvent $event){
+                $form = $event->getForm();
+                if($this->user !== null) {
+                    $form->get('author')->setData($this->user->getUsername());
+                }
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver): void
